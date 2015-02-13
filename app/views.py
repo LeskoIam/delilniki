af11dbf5e298f327e672fe904b3cc48a1f80b1bc
Lesko <matevzlesko@gmail.com>
2015-02-13 19:09:06 +0100

    - Removed old 'Show  all' view step 2

--- app/views.py
+++ app/views.py
@@ -80,12 +80,12 @@ def value_input():
                            data=data)
 
 
-@app.route("/showMk2")
-def showMk2():
+@app.route("/show")
+def show():
     sensor_data = db.session.query(SensorData, Sensor).join(Sensor).\
         order_by(SensorData.timestamp.desc()).order_by(Sensor.type).limit(18)
     # print sensor_data[0][0].timestamp
-    data = {"title": title_handler("Show All Mk2"),
+    data = {"title": title_handler("Show All"),
             "plot_heat": plot_tools.plot_heat(),
             "plot_water": plot_tools.plot_water(),
             "plot_heat_dividers": plot_tools.plot_last_heat_dividers(),
